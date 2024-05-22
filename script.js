4b38eeeee88a0242e79df0c8d386a9e90b1be292
LucasOsella <lucasosella01@gmail.com>
2024-05-22 15:36:22 -0300

    menu de nav

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function ver(){
+let contenido_info=document.getElementById("contenido_info")
+contenido_info.style.display='block';
+}
+
+function cerrar(){
+    let contenido_info=document.getElementById("contenido_info")
+    contenido_info.style.display='none'; 
+}
+
