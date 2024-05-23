4e3f3bc5a631015f0b6f70b1c0bd6820d76cb4ef
LucasOsella <lucasosella01@gmail.com>
2024-05-23 11:47:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
-
+/*funcion para mostrar el contenido del boton informacion*/
 function ver_info(){
 let contenido_info=document.getElementById("contenido_info")
 contenido_info.style.display='block';
 }
-function ver_form(){
-    let formulario=document.getElementById("formulario")
-    formulario.style.display='block';
-}
-
-
+/*funcion para cerrar el contenido del boton informacion*/
 function cerrar(){
     let contenido_info=document.getElementById("contenido_info")
     contenido_info.style.display='none'; 
 }
+/*funcion para mostrar el contenido del boton contacto*/
+function ver_form(){
+    let formulario=document.getElementById("formulario")
+    formulario.style.display='block';
+}
+/*funcion para el boton enviar del formulario*/
+
 
