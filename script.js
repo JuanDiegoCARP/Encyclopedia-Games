7425e04ac947e62b8c37e509e1f205c7f28cf3cb
LucasOsella <lucasosella01@gmail.com>
2024-05-23 11:45:46 -0300

    form agruegado
    agregado form, falta la función del botón enviar

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
 
-function ver(){
+function ver_info(){
 let contenido_info=document.getElementById("contenido_info")
 contenido_info.style.display='block';
 }
+function ver_form(){
+    let formulario=document.getElementById("formulario")
+    formulario.style.display='block';
+}
+
 
 function cerrar(){
     let contenido_info=document.getElementById("contenido_info")
