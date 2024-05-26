614300a3f2642c1c78b8be2cf8df939516c0cfa9
LucasOsella <lucasosella01@gmail.com>
2024-05-26 01:37:18 -0300

    flagerror

--- script.js
+++ script.js
@@ -35,21 +35,27 @@ const regExpNombre = /^[a-zA-Z ]{0,20}$/;
 const regExpEmail = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/
 
 function comprobar() {
+    let flagError=flase;
     const nombre=document.getElementById("nombre");
     const email=document.getElementById("email");
     const numero=document.getElementById("numero");
 
     if(nombre.value == ""|| nombre.value.length>20||!regExpNombre.test(nombre.value)){
         console.log("error nombre");
+        flagError=true;
     }
     if(numero.value==""| !numero.value.length > 10){
         console.log("error numero")
+        flagError=true;
 
     }
     if(email.value==""|| !regExpEmail.test(email.value)){
         console.log("Error email")
+        flagError=true;
     }
-    
+
+   if(flagError==false){
     console.log(nombre.value, numero.value, email.value)
+   }
     return false;
 }
