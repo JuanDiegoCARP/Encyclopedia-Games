47f00d88e2176815d6c8d139c1d53c5257e97dc3
JuanDiegoCARP <familiaalonso.jda@gmail.com>
2024-05-27 20:25:37 -0300

    Carrousel agregado

--- script.js
+++ script.js
@@ -93,3 +93,100 @@ numero.value=""
    return false;
 }
 
+window.onload = function(){
+
+// Variables
+
+const Imagenes = ['img/consolasjuegos.png','img/PS5.jpg','img/xbox.jpg'];
+
+const milesimas = 1000;
+
+let posicionActual = 0;
+
+let $botonRetroceder = document.querySelector('#retroceder');
+
+let $botonAvanzar = document.querySelector('#avanzar');
+
+let $imagen = document.querySelector('#imagen');
+
+let $botonPlay = document.querySelector('#play');
+
+let $botonStop = document.querySelector('#stop');
+
+let intervalo;
+
+
+function pasarFoto(){
+
+    if(posicionActual>= Imagenes.length -1){
+
+        posicionActual =0;
+
+    }   else {
+
+        posicionActual++;
+    } 
+    
+    renderizarImagen()
+    
+    }
+
+function retrocederFoto(){
+
+    if(posicionActual <= 0){
+        posicionActual = Imagenes.length -1;
+
+    }else{
+        posicionActual --;
+    }
+
+    renderizarImagen();
+
+}
+
+function renderizarImagen(){
+
+    $imagen.style.backgroundImage = `url(${Imagenes[posicionActual]})`;
+
+
+}
+
+function playIntervalo(){
+
+    intervalo = setInterval(pasarFoto,milesimas);
+
+    $botonAvanzar.setAttribute('disabled', true);
+    $botonRetroceder.setAttribute('disabled', true);
+    $botonPlay.setAttribute('disabled', true);
+    $botonStop.removeAttribute('disabled');
+
+
+}
+
+function PararIntervalo(){
+
+    clearInterval(intervalo);
+
+ //Activar botones
+
+$botonAvanzar.removeAttribute('disabled');
+$botonRetroceder.removeAttribute('disabled');
+$botonPlay.removeAttribute('disabled');
+$botonStop.setAttribute('disabled',true);
+
+// Eventos
+
+$botonAvanzar.addEventListener('click',pasarFoto);
+$botonRetroceder.addEventListener('click', retrocederFoto);
+$botonPlay.addEventListener('click', playIntervalo);
+$botonStop.addEventListener('click', PararIntervalo);
+// iniciar
+renderizarImagen();
+
+
+}
+
+
+}
+
+
