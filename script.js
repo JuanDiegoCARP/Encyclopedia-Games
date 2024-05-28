9e9f97072ff352894d6c45269204257a07734631
LucasOsella <lucasosella01@gmail.com>
2024-05-27 22:06:59 -0300

    update

--- script.js
+++ script.js
@@ -97,99 +97,77 @@ numero.value=""
 
 
 // Variables
-window.onload = function (){
-console.log("hola")
-const Imagenes = [
-    'img/consolasjuegos.png',
-    'img/PS5.jpg',
-    'img/xbox.jpg'];
+    window.onload = function () {
+        // Variables
+        const IMAGENES = [
+            'consolasjuegos.png',
+            'PS5.jpg',
+            'xbox.jpg'
+            ];
+        const TIEMPO = 1000;
+        let posicionActual = 0;
+        let $botonRetroceder = document.querySelector('#retroceder');
+        let $botonAvanzar = document.querySelector('#avanzar');
+        let $imagen = document.querySelector('#imagen');
+        let $botonPlay = document.querySelector('#play');
+        let $botonStop = document.querySelector('#stop');
+        let intervalo;
 
-const milesimas = 1000;
+        // Funciones
 
-let posicionActual = 0;
+        /**
+         * Funcion que cambia la foto en la siguiente posicion
+         */
+        function pasarFoto() {
+            if(posicionActual >= IMAGENES.length - 1) {
+                posicionActual = 0;
+            } else {
+                posicionActual++;
+            }
+            renderizarImagen();
+        }
 
-let $botonRetroceder = document.querySelector('#retroceder');
+        /**
+         * Funcion que cambia la foto en la anterior posicion
+         */
+        function retrocederFoto() {
+            if(posicionActual <= 0) {
+                posicionActual = IMAGENES.length - 1;
+            } else {
+                posicionActual--;
+            }
+            renderizarImagen();
+        }
 
-let $botonAvanzar = document.querySelector('#avanzar');
+        /**
+         * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+         */
+        function renderizarImagen () {
+            $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+        }
 
-let $imagen = document.querySelector('#imagen');
+        /**
+         * Activa el autoplay de la imagen
+         */
+        function playIntervalo() {
+            intervalo = setInterval(pasarFoto, TIEMPO);
+            // Desactivamos los botones de control
+            $botonPlay.setAttribute('disabled', true);
+            $botonStop.removeAttribute('disabled');
 
-let $botonPlay = document.querySelector('#play');
+        }
 
-let $botonStop = document.querySelector('#stop');
+        /**
+         * Para el autoplay de la imagen
+         */
 
-let intervalo;
-
-
-function pasarFoto(){
-
-    if(posicionActual>= Imagenes.length -1){
-
-        posicionActual =0;
-
-    }   else {
-
-        posicionActual++;
-    } 
-    
-    renderizarImagen()
-    
+        // Eventos
+        $botonAvanzar.addEventListener('click', pasarFoto);
+        $botonRetroceder.addEventListener('click', retrocederFoto);
+        // Iniciar
+        playIntervalo();
+        renderizarImagen();
     }
 
-function retrocederFoto(){
-
-    if(posicionActual <= 0){
-        posicionActual = Imagenes.length -1;
-
-    }else{
-        posicionActual --;
-    }
-
-    renderizarImagen();
-
-}
-
-function renderizarImagen(){
-
-    $imagen.style.backgroundImage = `url(${Imagenes[posicionActual]})`;
-
-
-}
-
-function playIntervalo(){
-
-    intervalo = setInterval(pasarFoto,milesimas);
-
-    $botonAvanzar.setAttribute('disabled', true);
-    $botonRetroceder.setAttribute('disabled', true);
-    $botonPlay.setAttribute('disabled', true);
-    $botonStop.removeAttribute('disabled');
-
-
-}
-
-function PararIntervalo(){
-
-    clearInterval(intervalo);
-
- //Activar botones
-
-$botonAvanzar.removeAttribute('disabled');
-$botonRetroceder.removeAttribute('disabled');
-$botonPlay.removeAttribute('disabled');
-$botonStop.setAttribute('disabled',true);
-}
-
-// Eventos
-
-$botonAvanzar.addEventListener('click',pasarFoto);
-$botonRetroceder.addEventListener('click', retrocederFoto);
-$botonPlay.addEventListener('click', playIntervalo);
-$botonStop.addEventListener('click', PararIntervalo);
-// iniciar
-
-renderizarImagen();
-
-}
 
 
